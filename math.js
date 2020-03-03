a505d1ebc11b0bf2678d9ea24d78cd04bc779b7a
Tumatauenga DIxon <tuma-dixon@live.nmit.ac.nz>
2020-03-03 14:46:26 +1300

    created math

---
+++ math.js
@@ -0,0 +1,11 @@
+var numOne=40;
+var numTwo=50;
+var numThree=60;
+var numFour=70;
+
+console.log("sum = " + ((numOne / numTwo)+(numThree / numOne)-(numFour % numFour)));
+console.log("div = " + (numOne / numTwo));
+
+
+
+// numOne, / numTwo, + numThree, / numOne, - numFour, % numFour
